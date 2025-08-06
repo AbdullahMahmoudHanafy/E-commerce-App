e004ff236d51a63d9549dc4683fdc2635b3cdd35
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-06 16:50:29 +0300

    Adding the Error 404 page

--- e-commerce/src/pages/Error 404/Error404.jsx
+++ e-commerce/src/pages/Error 404/Error404.jsx
@@ -2,8 +2,9 @@ import styles from './Error404.module.css';
 function Error404() {
     return (
         <div className={styles.mainContainer}>
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
+            <h1 className={styles.mainError}>404 Not Found</h1>
+            <p className={styles.paragraph}>Your visited page not found. You may go home page.</p>
+            <button className={styles.navigateButton}>Back to home page</button>
         </div>
     );
 }
