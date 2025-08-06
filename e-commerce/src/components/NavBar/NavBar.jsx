372393e568a1846f38db7228d029f3db37f7cbee
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-06 16:21:24 +0300

    adding the hr to the navbar

--- e-commerce/src/components/NavBar/NavBar.jsx
+++ e-commerce/src/components/NavBar/NavBar.jsx
@@ -5,33 +5,37 @@ import { faMagnifyingGlass, faHeart, faCartShopping } from '@fortawesome/free-so
 
 function NavBar() {
     return (
-        <div className={styles.mainDiv}>
-            <div className={styles.titleDiv}>
-                Exclusive
-            </div>
-            <div className={styles.pagesButtonsDiv}>
-                <button className={styles.navButton}>
-                    Home
-                </button>
-                <button className={styles.navButton}>
-                    Contact
-                </button>
-                <button className={styles.navButton}>
-                    About
-                </button>
-                <button className={styles.navButton}>
-                    Sign Up
-                </button>
-            </div>
-            <div className={styles.actionsDiv}>
-                <div className={styles.searchDiv}>
-                    <input type="search" placeholder="What are you looking for?"/>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} id={styles.searchIcon}/>
+        <>
+            <div className={styles.mainDiv}>
+                <div className={styles.titleDiv}>
+                    Exclusive
                 </div>
-                <FontAwesomeIcon icon={faHeart} className={styles.icon}/>
-                <FontAwesomeIcon icon={faCartShopping} className={styles.icon}/>
+                <div className={styles.pagesButtonsDiv}>
+                    <button className={styles.navButton}>
+                        Home
+                    </button>
+                    <button className={styles.navButton}>
+                        Contact
+                    </button>
+                    <button className={styles.navButton}>
+                        About
+                    </button>
+                    <button className={styles.navButton}>
+                        Sign Up
+                    </button>
+                </div>
+                <div className={styles.actionsDiv}>
+                    <div className={styles.searchDiv}>
+                        <input type="search" placeholder="What are you looking for?"/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} id={styles.searchIcon}/>
+                    </div>
+                    <FontAwesomeIcon icon={faHeart} className={styles.icon}/>
+                    <FontAwesomeIcon icon={faCartShopping} className={styles.icon}/>
+                </div>
+                
             </div>
-        </div>
+            <hr id={styles.hr}/>
+        </>
     );
 }
 
