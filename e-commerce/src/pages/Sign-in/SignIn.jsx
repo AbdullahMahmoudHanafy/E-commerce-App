1e4f7f238e8c2f562e4e9167f8fc163e9d5be78d
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-06 22:02:32 +0300

    adding the post function for login

--- e-commerce/src/pages/Sign-in/SignIn.jsx
+++ e-commerce/src/pages/Sign-in/SignIn.jsx
@@ -1,17 +1,52 @@
 import styles from './SignIn.module.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+    const navigate = useNavigate();
+    const [emailOrPhone, setEmailOrPhone] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // prevent page refresh
+
+        try {
+        const response = await fetch('https://fakestoreapi.com/auth/login', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            email: emailOrPhone,
+            password: password,
+            }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log('Login success:', data);
+            navigate('/');
+        } else {
+            console.error('Login failed:', data.message);
+            alert('Invalid credentials');
+        }
+        } catch (error) {
+        console.error('Error during login:', error);
+        }
+    };
+
     return (
         <div className={styles.mainContainer}>
             <img src="/images/Side Image.png" alt="A phone image" className={styles.sideImage}/>
             <div className={styles.formContainer}>
                 <p className={styles.header}>Log in to Exclusive</p>
                 <p className={styles.paragraph}>Enter your details below</p>
-                <form action="">
-                    <input type="email" placeholder='Email or Phone Number' className={styles.input}/>
-                    <input type="password" placeholder='Password' className={styles.input}/>
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="email" placeholder='Email or Phone Number' className={styles.input} value={emailOrPhone} onChange={(e) => setEmailOrPhone(e.target.value)} required/>
+                    <input type="password" placeholder='Password' className={styles.input} value={password} onChange={(e) => setPassword(e.target.value)} required/>
                     <div className={styles.buttonsContainer}>
-                        <button className={styles.submitButton}>Log in</button>
+                        <button type='submit' className={styles.submitButton}>Log in</button>
                         <button className={styles.forgetPasswordButton}>Forget Password?</button>
                     </div>
                 </form>
