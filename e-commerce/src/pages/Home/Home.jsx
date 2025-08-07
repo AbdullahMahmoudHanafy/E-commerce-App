db0f3c3557a3248b33a2c5f9d924c06dd2469d29
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-07 17:00:04 +0300

    adding the first section in the home page

--- e-commerce/src/pages/Home/Home.jsx
+++ e-commerce/src/pages/Home/Home.jsx
@@ -1,10 +1,31 @@
 import styles from './Home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
   return (
     <div className={styles.home}>
-      <h1>Welcome to Our E-Commerce Store</h1>
-      <p>Discover amazing products at unbeatable prices!</p>
+      <div className={styles.firstSection}>
+        <div className={styles.leftContainer}>
+            <button className={styles.titlesWithArrow}>
+                <p className={styles.leftSideTitle}>Woman’s Fashion</p>
+                <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <button className={styles.titlesWithArrow}>
+                <p className={styles.leftSideTitle}>Men’s Fashion</p>
+                <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Electronics</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Home & Lifestyle</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Medicine</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Sports & Outdoor</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Baby’s & Toys</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Groceries & Pets</p></button>
+            <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Health & Beauty</p></button>            
+        </div>
+        <div className={styles.verticalLine}></div>
+        <img className={styles.rightSideImg} src="/images/rightSideImg.png" alt="Iphone offer image" />
+      </div>
     </div>
   );
 }
