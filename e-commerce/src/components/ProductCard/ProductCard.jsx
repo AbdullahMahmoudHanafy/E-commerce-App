b90edee0a7548cdedaa5332d50450902c6c34928
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-07 20:56:37 +0300

    adding some dummy data for the products

--- e-commerce/src/components/ProductCard/ProductCard.jsx
+++ e-commerce/src/components/ProductCard/ProductCard.jsx
@@ -4,13 +4,13 @@ import { faHeart, faEye } from '@fortawesome/free-regular-svg-icons';
 import { faStar as solidStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
-function ProductCard({name, rating = 3}) {
+function ProductCard({product}) {
     return (
         <div className={styles.mainContainer}>
             <div className={styles.card}>
-                <img src="/images/gamePad.png" alt="Product" className={styles.image} />
+                <img src={product.image} alt="Product" className={styles.image} />
                 <div className={styles.addToCart}>Add to Cart</div>
-                <div className={styles.discountBadge}>-20%</div>
+                <div className={styles.discountBadge}>-{product.discount}%</div>
 
                 <button className={styles.button}>
                     <FontAwesomeIcon icon={faHeart} className={styles.icon}/>
@@ -20,21 +20,22 @@ function ProductCard({name, rating = 3}) {
                 </button>
             </div>
             <div className={styles.info}>
-                <p className={styles.title}>{name}</p>
+                <p className={styles.title}>{product.name}</p>
                 <div className={styles.priceContainer}>
-                    <p className={styles.finalPrice}>$39.99</p>
-                    <p className={styles.originalPrice}>$49.99</p>
+                    <p className={styles.finalPrice}>${product.price}</p>
+                    <p className={styles.originalPrice}>${product.lastPrice}</p>
                 </div>
                 <div className={styles.stars}>
                     {Array.from({ length: 5 }, (_, index) => {
-                        if (rating >= index + 1) {
+                        if (product.rating >= index + 1) {
                         return <FontAwesomeIcon key={index} icon={solidStar} />;
-                        } else if (rating > index && rating < index + 1) {
+                        } else if (product.rating > index && product.rating < index + 1) {
                         return <FontAwesomeIcon key={index} icon={faStarHalfAlt} />;
                         } else {
                         return <FontAwesomeIcon key={index} icon={regularStar} />;
                         }
                     })}
+                    <span className={styles.ratingCount}>  ({product.ratingCount})</span>
                 </div>
             </div>
         </div>
--- e-commerce/src/pages/Home/Home.jsx
+++ e-commerce/src/pages/Home/Home.jsx
@@ -5,9 +5,11 @@ import { ProductCard } from '../../components';
 
 function Home() {
     const products = [
-        { name: "HAVIT HV-G92 Gamepad" },
-        { name: "AK-900 Wired Keyboard" },
-        { name: "IPS LCD Gaming Monitor" }
+        { name: "HAVIT HV-G92 Gamepad", rating: 4.5, ratingCount: 150, price: 120, discount: 40, lastPrice: 160, image: "/images/products/gamepad.png" },
+        { name: "AK-900 Wired Keyboard", rating: 4.0, ratingCount: 200, price: 960, discount: 35, lastPrice: 1160, image: "/images/products/keyboard.png" },
+        { name: "IPS LCD Gaming Monitor", rating: 4.2, ratingCount: 180, price: 370, discount: 30, lastPrice: 400, image: "/images/products/ipsscreen.png" },
+        { name: "S-Series Comfort Chair ", rating: 4.6, ratingCount: 99, price: 375, discount: 25, lastPrice: 400, image: "/images/products/chair.png" },
+        { name: "IPS LCD Gaming Monitor", rating: 4.2, ratingCount: 180, price: 370, discount: 30, lastPrice: 400, image: "/images/products/ipsscreen.png" }
     ]
   return (
     <div className={styles.home}>
@@ -50,7 +52,7 @@ function Home() {
             <div className={styles.content}>
                 <div className={styles.productCardsContainer}>
                     {products.map((product, index) => (
-                        <ProductCard key={index} name={product.name} />
+                        <ProductCard key={index} product={product}/>
                     ))}
                 </div>
             </div>
