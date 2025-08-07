11cb002ef320594882b77e8a6cdddf3c95897a1b
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-07 22:53:41 +0300

    adding the service section and making it responsive

--- e-commerce/src/pages/Home/Home.jsx
+++ e-commerce/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import styles from './Home.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTruck, faHeadphones, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 import { ProductCard } from '../../components';
 import { useRef } from 'react';
 
@@ -91,6 +92,23 @@ function Home() {
                 <img className={styles.mediumImg} src="/images/womenFrame.png" alt="" />
                 <img src="/images/speakersFrame.png" alt="" />
                 <img src="/images/perfumeFrame.png" alt="" />
+            </div>
+        </div>
+        <div className={styles.serviceSection}>
+            <div className={styles.featureDiv}>
+                <div className={styles.buttonDiv}><button><FontAwesomeIcon icon={faTruck} className={styles.serviceIcon}/></button></div>
+                <p className={styles.serviceTitle}>FREE AND FAST DELIVERY</p>
+                <p>Free delivery for all orders over $140</p>
+            </div>
+            <div className={styles.featureDiv}>
+                <div className={styles.buttonDiv}><button><FontAwesomeIcon icon={faHeadphones} className={styles.serviceIcon}/></button></div>
+                <p className={styles.serviceTitle}>24/7 CUSTOMER SERVICE</p>
+                <p>Friendly 24/7 customer support</p>
+            </div>
+            <div className={styles.featureDiv}>
+                <div className={styles.buttonDiv}><button><FontAwesomeIcon icon={faSquareCheck} className={styles.serviceIcon}/></button></div>
+                <p className={styles.serviceTitle}>MONEY BACK GUARANTEE</p>
+                <p>We return money within 30 days</p>
             </div>
         </div>
     </div>
