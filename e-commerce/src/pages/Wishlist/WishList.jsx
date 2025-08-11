514d8b1a6f6f1af379cc3a16eb10436b9642ed85
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-08-11 17:47:23 +0300

    adding the content container in wishlist page

--- e-commerce/src/pages/Wishlist/WishList.jsx
+++ e-commerce/src/pages/Wishlist/WishList.jsx
@@ -1,6 +1,17 @@
 import styles from "./WishList.module.css";
+import { ProductCard } from "../../components";
 
 function WishList() {
+    const products = [
+        { name: "HAVIT HV-G92 Gamepad", rating: 4.5, ratingCount: 150, price: 120, discount: 40, lastPrice: 160, image: "/images/products/gamepad.png" },
+        { name: "AK-900 Wired Keyboard", rating: 4.0, ratingCount: 200, price: 960, discount: 35, lastPrice: 1160, image: "/images/products/keyboard.png" },
+        { name: "IPS LCD Gaming Monitor", rating: 4.2, ratingCount: 180, price: 370, discount: 30, lastPrice: 400, image: "/images/products/ipsscreen.png" },
+        { name: "S-Series Comfort Chair ", rating: 4.6, ratingCount: 99, price: 375, discount: 25, lastPrice: 400, image: "/images/products/chair.png" },
+        { name: "IPS LCD Gaming Monitor", rating: 4.2, ratingCount: 180, price: 370, discount: 30, lastPrice: 400, image: "/images/products/ipsscreen.png" },
+        { name: "HAVIT HV-G92 Gamepad", rating: 4.5, ratingCount: 150, price: 120, discount: 40, lastPrice: 160, image: "/images/products/gamepad.png" },
+        { name: "AK-900 Wired Keyboard", rating: 4.0, ratingCount: 200, price: 960, discount: 35, lastPrice: 1160, image: "/images/products/keyboard.png" },
+        { name: "IPS LCD Gaming Monitor", rating: 4.2, ratingCount: 180, price: 370, discount: 30, lastPrice: 400, image: "/images/products/ipsscreen.png" }
+    ]
     return (
         <div className={styles.mainContainer}>
             <div className={styles.headerContainer}>
@@ -12,7 +23,9 @@ function WishList() {
                 </button>
             </div>
             <div className={styles.productContainer}>
-                
+                {products.map((product, index) => (
+                    <ProductCard key={index} product={product} />
+                ))}
             </div>
         </div>
     );
