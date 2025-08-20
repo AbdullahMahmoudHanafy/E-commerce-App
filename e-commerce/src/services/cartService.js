8da376015ad19583efac94bf1cff078baad91729
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-08-20 23:48:50 +0300

    adding add to cart service function

--- e-commerce/src/services/cartService.js
+++ e-commerce/src/services/cartService.js
@@ -2,4 +2,14 @@ function loadCart() {
     return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
-export { loadCart };
+function addTocart(product) {
+    const id = product.id;
+    let wishlist = JSON.parse(localStorage.getItem('cart')) || [];
+    if (wishlist.some(item => item.id === id)) {
+        return; // already in wishlist, do nothing
+    }
+    wishlist.push(product);
+    localStorage.setItem('cart', JSON.stringify(wishlist));
+}
+
+export { loadCart, addTocart };
--- e-commerce/src/services/wishListServices.js
+++ e-commerce/src/services/wishListServices.js
@@ -1,31 +1,31 @@
-    function loadWishlist() {
-        return JSON.parse(localStorage.getItem("wishlist")) || [];
-    }
-    
-    function saveToWishlist(product) {
-        const id = product.id;
-        let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
-        if (wishlist.some(item => item.id === id)) {
-            return; // already in wishlist, do nothing
-        }
-        wishlist.push(product);
-        localStorage.setItem('wishlist', JSON.stringify(wishlist));
-    }
+function loadWishlist() {
+    return JSON.parse(localStorage.getItem("wishlist")) || [];
+}
 
-    function removeFromWishlist(productId) {
-        let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
-        wishlist = wishlist.filter(item => item.id !== productId);
-        localStorage.setItem('wishlist', JSON.stringify(wishlist));
+function saveToWishlist(product) {
+    const id = product.id;
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+    if (wishlist.some(item => item.id === id)) {
+        return; // already in wishlist, do nothing
     }
+    wishlist.push(product);
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+}
 
-    function removeFromWishlistAndRefresh(productId) {
-        removeFromWishlist(productId);
-        return loadWishlist();
-    }
+function removeFromWishlist(productId) {
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+    wishlist = wishlist.filter(item => item.id !== productId);
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+}
 
-    function saveToWishlistAndRefresh(product) {
-        saveToWishlist(product);
-        return loadWishlist();
-    }
+function removeFromWishlistAndRefresh(productId) {
+    removeFromWishlist(productId);
+    return loadWishlist();
+}
 
-    export { loadWishlist, removeFromWishlist, saveToWishlist, removeFromWishlistAndRefresh, saveToWishlistAndRefresh };
+function saveToWishlistAndRefresh(product) {
+    saveToWishlist(product);
+    return loadWishlist();
+}
+
+export { loadWishlist, removeFromWishlist, saveToWishlist, removeFromWishlistAndRefresh, saveToWishlistAndRefresh };
