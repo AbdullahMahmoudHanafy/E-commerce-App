61de8b2db7c85fc589e79c21b07681fac7554546
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-08-20 23:50:36 +0300

    updating service's index file

--- e-commerce/src/services/index.js
+++ e-commerce/src/services/index.js
@@ -1,5 +1,6 @@
 import { saveToWishlist, loadWishlist, removeFromWishlist, saveToWishlistAndRefresh, removeFromWishlistAndRefresh } from "./wishListServices";
 import { getCurrentUser, setCurrentUser } from "./userService";
 import { getProduct, getAllProducts } from "./ProductsService";
+import { loadCart, addTocart, removeFromcart } from "./cartService";
 
-export { saveToWishlist, loadWishlist, removeFromWishlist, saveToWishlistAndRefresh, removeFromWishlistAndRefresh, getCurrentUser, setCurrentUser, getProduct, getAllProducts };
+export { saveToWishlist, loadWishlist, removeFromWishlist, saveToWishlistAndRefresh, removeFromWishlistAndRefresh, getCurrentUser, setCurrentUser, getProduct, getAllProducts, loadCart, addTocart, removeFromcart };
