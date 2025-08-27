153ce308eb5f035e1c426c21f2568889ab1709df
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-08-27 22:30:47 +0300

    refactoring home page css

--- e-commerce/src/pages/Home/Home.jsx
+++ e-commerce/src/pages/Home/Home.jsx
@@ -38,69 +38,71 @@ function Home() {
     }
   return (
     <div className={styles.home}>
-        <div className={styles.firstSection}>
-            <div className={styles.leftContainer}>
-                <button className={styles.titlesWithArrow}>
-                    <p className={styles.leftSideTitle}>Woman’s Fashion</p>
-                    <FontAwesomeIcon icon={faAngleRight} />
-                </button>
-                <button className={styles.titlesWithArrow}>
-                    <p className={styles.leftSideTitle}>Men’s Fashion</p>
-                    <FontAwesomeIcon icon={faAngleRight} />
-                </button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Electronics</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Home & Lifestyle</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Medicine</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Sports & Outdoor</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Baby’s & Toys</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Groceries & Pets</p></button>
-                <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Health & Beauty</p></button>            
+        <div className={styles.colDiv}>
+            <div className={styles.firstSection}>
+                <div className={styles.leftContainer}>
+                    <button className={styles.titlesWithArrow}>
+                        <p className={styles.leftSideTitle}>Woman’s Fashion</p>
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </button>
+                    <button className={styles.titlesWithArrow}>
+                        <p className={styles.leftSideTitle}>Men’s Fashion</p>
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Electronics</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Home & Lifestyle</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Medicine</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Sports & Outdoor</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Baby’s & Toys</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Groceries & Pets</p></button>
+                    <button className={styles.titlesWithoutArrow}><p className={styles.leftSideTitle}>Health & Beauty</p></button>            
+                </div>
+                <div className={styles.verticalLine}></div>
+                <img className={styles.rightSideImg} src="/images/rightSideImg.png" alt="Iphone offer image" />
             </div>
-            <div className={styles.verticalLine}></div>
-            <img className={styles.rightSideImg} src="/images/rightSideImg.png" alt="Iphone offer image" />
-        </div>
-        <div className={styles.secondSection}>
-            <div className={styles.sectionTitle}>
-                <div className={styles.dummyDiv}></div>
-                <p className={styles.redTitle}>Today's</p>
+            <div className={styles.secondSection}>
+                <div className={styles.sectionTitle}>
+                    <div className={styles.dummyDiv}></div>
+                    <p className={styles.redTitle}>Today's</p>
+                </div>
+                <div className={styles.secondRow}>
+                    <div className={styles.secondRowTitlesContainer}>
+                        <p className={styles.secondRowTitles}>Flash Sales</p>
+                        <p className={styles.secondRowTitles}>03<p className={styles.redMarkers}> : </p>23<p className={styles.redMarkers}> : </p>19<p className={styles.redMarkers}> : </p>56</p>
+                    </div>
+                    <div className={styles.navigators}>
+                        <button className={styles.navigationButton} onClick={() => scroll('left')}><FontAwesomeIcon icon={faAngleLeft} /></button>
+                        <button className={styles.navigationButton} onClick={() => scroll('right')}><FontAwesomeIcon icon={faAngleRight} /></button>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.productCardsContainer} ref={scrollRef}>
+                        {products && products.map((product, index) => (
+                            <ProductCard key={index} product={product} onAddToWishlist={saveToWishlist}/>
+                        ))}
+                    </div>
+                </div>
+                <div className={styles.buttonsContainer}>
+                    <button className={styles.viewAllButton} onClick={navigateToProductsPage}>View All Products</button>
+                </div>
             </div>
-            <div className={styles.secondRow}>
+            <div className={styles.thirdSection}>
+                <div className={styles.sectionTitle}>
+                    <div className={styles.dummyDiv}></div>
+                    <p className={styles.redTitle}>Featured</p>
+                </div>
                 <div className={styles.secondRowTitlesContainer}>
-                    <p className={styles.secondRowTitles}>Flash Sales</p>
-                    <p className={styles.secondRowTitles}>03<p className={styles.redMarkers}> : </p>23<p className={styles.redMarkers}> : </p>19<p className={styles.redMarkers}> : </p>56</p>
+                    <p className={styles.secondRowTitles}>New Arrival</p>
                 </div>
-                <div className={styles.navigators}>
-                    <button className={styles.navigationButton} onClick={() => scroll('left')}><FontAwesomeIcon icon={faAngleLeft} /></button>
-                    <button className={styles.navigationButton} onClick={() => scroll('right')}><FontAwesomeIcon icon={faAngleRight} /></button>
-                </div>
-            </div>
-            <div className={styles.content}>
-                <div className={styles.productCardsContainer} ref={scrollRef}>
-                    {products && products.map((product, index) => (
-                        <ProductCard key={index} product={product} onAddToWishlist={saveToWishlist}/>
-                    ))}
+                <div className={styles.imageContainer}>
+                    <img className={styles.bigImg} src="/images/playStationFrame.png" alt="" />
+                    <img className={styles.mediumImg} src="/images/womenFrame.png" alt="" />
+                    <img src="/images/speakersFrame.png" alt="" />
+                    <img src="/images/perfumeFrame.png" alt="" />
                 </div>
             </div>
-            <div className={styles.buttonsContainer}>
-                <button className={styles.viewAllButton} onClick={navigateToProductsPage}>View All Products</button>
-            </div>
+            <ServicesList />    
         </div>
-        <div className={styles.thirdSection}>
-            <div className={styles.sectionTitle}>
-                <div className={styles.dummyDiv}></div>
-                <p className={styles.redTitle}>Featured</p>
-            </div>
-            <div className={styles.secondRowTitlesContainer}>
-                <p className={styles.secondRowTitles}>New Arrival</p>
-            </div>
-            <div className={styles.imageContainer}>
-                <img className={styles.bigImg} src="/images/playStationFrame.png" alt="" />
-                <img className={styles.mediumImg} src="/images/womenFrame.png" alt="" />
-                <img src="/images/speakersFrame.png" alt="" />
-                <img src="/images/perfumeFrame.png" alt="" />
-            </div>
-        </div>
-        <ServicesList />
     </div>
   );
 }
