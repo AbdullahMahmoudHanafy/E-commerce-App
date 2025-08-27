59e26da9c8ba1ec5cb76d46d55ba9a59de65a706
Abdullah Mahmoud <almatador.egy@gmail.com>
2025-08-27 21:04:45 +0300

    adding the details section to about page

--- e-commerce/src/pages/About/About.jsx
+++ e-commerce/src/pages/About/About.jsx
@@ -1,7 +1,28 @@
 import styles from './About.module.css';
-import { ServicesList } from '../../components';
+import { ServicesList, ServiceIcon } from '../../components';
+import { faSackDollar, faShop, faDollarSign, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
+    const details = [{
+        icon: faShop,
+        Number: '10.5K',
+        title: 'Sallers active our site'
+    },
+    {
+        icon: faDollarSign,
+        Number: '33K',
+        title: 'Mopnthly Produduct Sale'
+    },
+    {
+        icon: faShoppingBag,
+        Number: '45.5K',
+        title: 'Customer active in our site'
+    },
+    {
+        icon: faSackDollar,
+        Number: '25K',
+        title: 'Anual gross sale in our site'
+    }]
     return (
         <div className={styles.mainContainer}>
             <div className={styles.colDiv}>
@@ -15,6 +36,19 @@ function About() {
                         <p>Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
                     </div>
                     <img src="/images/aboutImg.png" alt="" />
+                </div>
+                <div className={styles.detailsDiv}>
+                    {
+                        details.map((detail, index) => (
+                            <div key={index} className={styles.detailDiv}>
+                                <ServiceIcon icon={detail.icon} />
+                                <div className={styles.textDetailDiv}>
+                                    <h3 className={styles.numberText}>{detail.Number}</h3>
+                                    <p className={styles.titleText}>{detail.title}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
                 <ServicesList />
             </div>
